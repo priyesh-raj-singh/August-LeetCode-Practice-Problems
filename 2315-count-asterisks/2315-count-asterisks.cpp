6b605f42f66d9983131174dedc31773a6a86bbbd
Priyesh Raj Singh <65354440+priyesh-raj-singh@users.noreply.github.com>
2022-08-02 03:08:23 +0530

    Time: 3 ms (62.64%), Space: 6.5 MB (37.59%) - LeetHub

---
+++ 2315-count-asterisks/2315-count-asterisks.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int countAsterisks(string s) {
+        int ans = 0 ;
+        int as = 0;
+        for(int i = 0 ; i<s.size() ; i++){
+            if(s[i]=='|')
+                as++;
+            
+            if(as%2==0 and s[i] == '*')
+                ans++;
+        }
+        return ans;
+        
+    }
+};
