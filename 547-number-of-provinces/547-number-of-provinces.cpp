87daef2f0bfe2c4bf2e4e7e224a3e8fe398e9615
Priyesh Raj Singh <65354440+priyesh-raj-singh@users.noreply.github.com>
2022-09-07 21:49:08 +0530

    Time: 60 ms (13.71%), Space: 15.3 MB (11.45%) - LeetHub

--- 547-number-of-provinces/547-number-of-provinces.cpp
+++ 547-number-of-provinces/547-number-of-provinces.cpp
@@ -1,42 +1,34 @@
 class Solution {
-  private: 
-    // dfs traversal function 
-    void dfs(int node, vector<int> adjLs[], vector<int> &vis) {
-        // mark the more as visited
-        vis[node] = 1; 
-        for(auto it: adjLs[node]) {
-            if(!vis[it]) {
-                dfs(it, adjLs, vis); 
+    private:
+        void dfs(int node , vector<int> ls[] , vector<int> &vis){
+            vis[node] = 1;
+            for(auto it: ls[node]){
+                if(!vis[it])
+                    dfs(it , ls , vis);
             }
         }
-    }
-  public:
-    int findCircleNum(vector<vector<int>>& adj)  {
-        int V = adj.size();
-        vector<int> adjLs[V]; 
-        
-        // to change adjacency matrix to list 
-        for(int i = 0;i<V;i++) {
-            for(int j = 0;j<V;j++) {
-                // self nodes are not considered
-                if(adj[i][j] == 1 && i != j) {
-                    adjLs[i].push_back(j); 
-                    adjLs[j].push_back(i); 
+public:
+    int findCircleNum(vector<vector<int>>& isConnected) {
+        int V = isConnected.size();
+        vector<int> ls[V];
+        int count = 0;
+        for(int i = 0 ; i<V ; i++){
+            for(int j = 0 ; j<V ; j++){
+                if(isConnected[i][j]==1){
+                    ls[i].push_back(j);
+                    ls[j].push_back(i);
                 }
             }
         }
-        vector<int> vis(V , 0); 
-        int cnt = 0; 
-        for(int i = 0;i<V;i++) {
-            // if the node is not visited
-            if(!vis[i]) {
-                // counter to count the number of provinces 
-                cnt++;
-               dfs(i, adjLs, vis); 
+        
+        vector<int> vis(V , 0);
+        for(int i = 0 ; i<V ; i++){
+            if(!vis[i]){
+                count++;
+                dfs(i , ls , vis);
             }
         }
-        return cnt; 
+        return count;
         
     }
-    
 };
