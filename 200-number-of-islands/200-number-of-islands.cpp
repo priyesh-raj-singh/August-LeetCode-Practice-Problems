dcc18e3e60dc10a4df508559132d3b4e80e06c0b
Priyesh Raj Singh <65354440+priyesh-raj-singh@users.noreply.github.com>
2022-09-08 18:30:58 +0530

    Time: 82 ms (17.32%), Space: 13.4 MB (26.35%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.cpp
+++ 200-number-of-islands/200-number-of-islands.cpp
@@ -1,46 +1,42 @@
 class Solution {
-    private:
-void bfs(int row , int col , vector<vector<int>> & vis , vector<vector<char>> & grid){
-    vis[row][col] = 1;
-        
-    int n = grid.size();
-    int m = grid[0].size();
-    queue<pair<int , int>> q;
-    q.push({row , col});
-    while(!q.empty()){
-        int r = q.front().first;
-        int c = q.front().second;
-        q.pop();
-        int delta_row[] = {-1, 0, 1, 0};
-        int delta_col[] = {0, 1, 0, -1};
-        
-        for(int i = 0 ; i<4 ; i++){
-            int nrow = r + delta_row[i];
-            int ncol = c + delta_col[i];
-            if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && grid[nrow][ncol]=='1' && !vis[nrow][ncol]){
-                        vis[nrow][ncol] = 1;
-                        q.push({nrow , ncol});
-                    }
-        }
-    }
-}
 public:
-    int numIslands(vector<vector<char>>& grid) {
+     void dfs(int row,int col,vector<vector<char>> &grid,vector<vector<int>> & visited,vector<int> & dr,vector<int> &dc)
+    {
+        visited[row][col] = 1;
         int n = grid.size();
         int m = grid[0].size();
-        int count = 0;
-        vector<vector<int>> vis (n , vector<int>(m , 0));
         
-        for(int i = 0 ; i<n ; i++){
-            for(int j = 0 ; j<m ; j++){
-                if(!vis[i][j] and grid[i][j] == '1'){
-                    count++;
-                    bfs(i , j , vis , grid);
-                }
-            }
+        
+        for(int i=0;i<4;i++)
+        {
+          int nrow=row+dr[i],ncol=col+dc[i];
+                
+              if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && grid[nrow][ncol]=='1' && !visited[nrow][ncol])  
+              { 
+                  dfs(nrow,ncol,grid,visited,dr,dc);
+              }  
         }
-        return count;
-        
-        
+    }
+    
+    int numIslands(vector<vector<char>>& grid) {
+        int n=grid.size();
+      int m=grid[0].size();  
+      vector<vector<int>> visited(n,vector<int>(m,0));
+      int cnt=0;  
+      vector<int> dr,dc;
+      dr={-1,1,0,0};
+       dc={0,0,-1,1}; 
+      for(int row =0;row<n;row++)
+      {
+          for(int col=0;col<m;col++)
+          {
+              if(!visited[row][col] && grid[row][col]=='1')
+              {
+                  cnt++;
+                  dfs(row,col,grid,visited,dr,dc);
+              }
+          }
+      }
+    return cnt;    
     }
 };
