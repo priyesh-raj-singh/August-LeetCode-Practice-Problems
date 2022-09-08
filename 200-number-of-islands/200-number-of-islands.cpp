f4b2d495fa02c5c57e1fd233451714b48f1f3253
Priyesh Raj Singh <65354440+priyesh-raj-singh@users.noreply.github.com>
2022-09-08 16:44:46 +0530

    Time: 46 ms (69.96%), Space: 19.2 MB (9.32%) - LeetHub

---
+++ 200-number-of-islands/200-number-of-islands.cpp
@@ -0,0 +1,60 @@
+class Solution
+{
+private:
+    void bfs(int row, int col, vector<vector<int>> &visited, vector<vector<char>> &grid)
+    {
+        int n = grid.size();
+        int m = grid[0].size();
+
+        visited[row][col] = 1;
+        queue<pair<int, int>> q;
+        q.push({row, col});
+
+        while (!q.empty())
+        {
+            int r = q.front().first;
+            int c = q.front().second;
+            q.pop();
+             int delta_row[] = {-1, 0, 1, 0};
+            int delta_col[] = {0, 1, 0, -1};
+
+            
+            for (int i = 0; i < 4; i++)
+            {
+                int neighbour_row = r + delta_row[i];
+                int neighbour_col = c + delta_col[i];
+
+                
+                if (neighbour_row >= 0 && neighbour_row < n && neighbour_col >= 0 && neighbour_col < m)
+                {
+                    // check for land and prev visited
+                    if (grid[neighbour_row][neighbour_col] == '1' && !visited[neighbour_row][neighbour_col])
+                    {
+                        visited[neighbour_row][neighbour_col] = 1;
+                        q.push({neighbour_row, neighbour_col});
+                    }
+                }
+            }
+        }
+    }
+
+public:
+    int numIslands(vector<vector<char>> &grid)
+    {
+        int n = grid.size();
+        int m = grid[0].size();
+        vector<vector<int>> visited(n, vector<int>(m, 0));
+        int ans = 0;
+       
+
+        for (int r = 0; r < n; r++)
+            for (int c = 0; c < m; c++)
+                if (!visited[r][c] && grid[r][c] == '1')
+                {
+                    ++ans;
+                    bfs(r, c, visited, grid);
+                }
+
+        return ans;
+    }
+};
